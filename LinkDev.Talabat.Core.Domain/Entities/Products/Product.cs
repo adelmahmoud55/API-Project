eb568eca6d2869616ae940b0ae72c51bce528711
adelmahmoud55 <adelmaahmoud@gmail.com>
2024-10-05 11:41:52 +0300

    Part 03 Product Module - Entities.

---
+++ LinkDev.Talabat.Core.Domain/Entities/Products/Product.cs
@@ -0,0 +1,29 @@
+﻿using LinkDev.Talabat.Core.Domain.Comman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Products
+{
+    public class Product  : BaseEntity<int>
+    {
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        public string? PictureUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+
+        public int? BrandId { get; set; } // Foriegn Key --> ProductBrand Entity. {? means Optional}
+        public ProductBrand? Brand { get; set; } // null cuz when delete the brand the product still exist, and BrandId is set to null.
+
+
+        public int? CategoryId { get; set; } // Foriegn Key --> ProductCategory Entity. {? means Optional}
+        public ProductCategory? Category { get; set; } // null cuz when delete the category the product still exist, and CategoryId is set to null.
+
+    }
+}
