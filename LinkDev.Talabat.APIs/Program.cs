e7a6755843ebbc3fb638d40138af6e30e70a04cb
adelmahmoud55 <adelmaahmoud@gmail.com>
2024-10-07 11:38:36 +0300

    Part 05 Apply Migrations (Update-Database).

--- LinkDev.Talabat.APIs/Program.cs
+++ LinkDev.Talabat.APIs/Program.cs
@@ -1,12 +1,14 @@
 
 using LinkDev.Talabat.Infrastructure.Persistence;
+using LinkDev.Talabat.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace LinkDev.Talabat.APIs
 {
     public class Program
     {
         // Entry point for the application.
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var webApplicationBuilder = WebApplication.CreateBuilder(args);
            
@@ -30,6 +32,28 @@ namespace LinkDev.Talabat.APIs
             #endregion
 
             var app = webApplicationBuilder.Build();
+
+           using  var Scope =  app.Services.CreateAsyncScope();
+                  var Services = Scope.ServiceProvider;
+                  var dbContext = Services.GetRequiredService<StoreContext>();
+                   // ask runtime enviroment  for an object from "StoreContext" Service explicitly.
+                    
+                  var LoggerFactory = Services.GetRequiredService<ILoggerFactory>();
+
+            try 
+            {
+                var PendingMigrations = dbContext.Database.GetPendingMigrations(); // Get Pending Migrations.
+                //we can use MigrateAsync direct but time consum
+
+
+                if (PendingMigrations.Any())
+                    await  dbContext.Database.MigrateAsync(); // Update Database 
+            }
+            catch (Exception ex)
+            {
+                var logger = LoggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An error occurred during applying the migratings.");
+            }
 
             #region Configure Kestrel Middlewares
 
