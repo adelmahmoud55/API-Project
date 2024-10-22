031fa4baa8757bc29c04532a9ce286bc62e4bd19
adelmahmoud55 <adelmaahmoud@gmail.com>
2024-10-23 01:31:38 +0300

    Part 01 Identity Module - Entities.

---
+++ LinkDev.Talabat.Core.Domain/Identity/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Identity
+{
+    public class Address /*: BaseEntity<int>*/ // we will not use this cuz we will make a new seperate database seprate from store Context
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required string Street { get; set; }
+
+        public required string City { get; set; }
+
+        public required string Country { get; set; }
+
+        public int UserId { get; set; }
+
+        public required ApplicationUser AppUser { get; set; }
+    }
+}
+
---
+++ LinkDev.Talabat.Core.Domain/Identity/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Identity
+{
+    // here we custmize on one of the 7 Identitties "IdentityUser"
+    public class ApplicationUser : IdentityUser<string> //The default implementation of Microsoft.AspNetCore.Identity.IdentityUser`1 which
+                                                        //     uses a string as a primary key. so if we inherit from non generic IdentityUser its the same cuz IdentityUser inherit from IdentityUser<string>
+    {
+        public required string DisplayName { get; set; }
+
+        public Address? Address { get; set; }
+    }
+}
