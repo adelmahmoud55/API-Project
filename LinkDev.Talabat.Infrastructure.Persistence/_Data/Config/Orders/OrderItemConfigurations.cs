2618c6055744ee382da4cdea6baed477561b1d76
adelmahmoud55 <adelmaahmoud@gmail.com>
2025-06-20 16:50:30 +0300

    Part 02 Order Module - Configurartion Classes

---
+++ LinkDev.Talabat.Infrastructure.Persistence/_Data/Config/Orders/OrderItemConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using LinkDev.Talabat.Core.Domain.Entities.Orders;
+using LinkDev.Talabat.Infrastructure.Persistence.Data.Config.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Infrastructure.Persistence._Data.Config.Orders
+{
+    internal class OrderItemConfigurations :BaseAuditableEntityConfigurations<OrderItem, int>
+    {
+        public override void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            base.Configure(builder);
+
+            builder.OwnsOne(item => item.Product, product => product.WithOwner());
+            
+
+            builder.Property(item => item.Price)
+                .HasColumnType("decimal(8,2)");
+        }
+    }
+}
