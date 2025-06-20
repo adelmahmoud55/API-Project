f28c656bf2d52c35cc8490cb62a5c740c1e4c11b
adelmahmoud55 <adelmaahmoud@gmail.com>
2025-06-20 16:14:45 +0300

    Part 01 Order Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Orders/ProductItemOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Orders
+{
+    public class ProductItemOrder
+    {
+        public int ProductId { get; set; }
+        public required string ProductName { get; set; }
+       
+        public required string PictureUrl { get; set; } 
+    }
+}
