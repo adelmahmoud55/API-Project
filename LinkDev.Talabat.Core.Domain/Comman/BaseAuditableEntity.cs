fae509d32ac5383c46ff68edab7501a4aac89b2a
adelmahmoud55 <adelmaahmoud@gmail.com>
2025-06-22 00:51:49 +0300

    Part 08 GetOrdersForUser, GetOrderForUser, GetDeliveryMethods endpoints

--- LinkDev.Talabat.Core.Domain/Comman/BaseAuditableEntity.cs
+++ LinkDev.Talabat.Core.Domain/Comman/BaseAuditableEntity.cs
@@ -13,13 +13,13 @@ namespace LinkDev.Talabat.Core.Domain.Comman
     {
 
 
-        public string CreatedBy { get; set; } = null!;
+        public string? CreatedBy { get; set; } = null!;
 
-        public  DateTime CreatedOn { get; set; } 
+        public  DateTime? CreatedOn { get; set; } 
 
-        public  string LastModifiedBy { get; set; } = null!;
+        public  string? LastModifiedBy { get; set; } = null!;
 
-        public DateTime LastModifiedOn { get; set; } 
+        public DateTime?LastModifiedOn { get; set; } 
     }
 
 
--- LinkDev.Talabat.Infrastructure/BasketRepository/BasketRepository.cs
+++ LinkDev.Talabat.Infrastructure/BasketRepository/BasketRepository.cs
@@ -14,9 +14,9 @@ namespace LinkDev.Talabat.Infrastructure.BasketRepository
     {
         private readonly IDatabase _database;
 
-        public BasketRepository(IConnectionMultiplexer redis)
+        public BasketRepository(/*IConnectionMultiplexer redis*/)
         {
-            _database = redis.GetDatabase();
+            //_database = redis.GetDatabase();
             
         }
 
